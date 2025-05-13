119a3548479ede673b98238bca01e9e6c7f7433d
Loriek-cyber <arjel.buzi1@gmail.com>
2025-05-13 12:44:50 +0200

    Funzionalità del Database Aggiunte

--- src/main/java/utils/DBConnector.java
+++ src/main/java/utils/DBConnector.java
@@ -6,17 +6,30 @@ import java.sql.SQLException;
 
 public class DBConnector {
 
+    // Database connection details
     private static final String DB_URL = "jdbc:mysql://mysql-omniride-1412-omniride.i.aivencloud.com:24112/defaultdb?ssl-mode=REQUIRED";
     private static final String DB_USER = "avnadmin";
-    private static final String DB_PASSWORD = "AVNS_gdOiYGNkxzpdQpoIiko";
 
     public static Connection getConnection() throws SQLException {
+        // Retrieve the password from the system environment variable
+        String dbPassword = System.getenv("DB_PASSWORD");
+        
+
+        if (dbPassword == null) {
+        	throw new SQLException("Database password not found in environment variables.");
+        }
+        
+        // Print Password for debugging (remove in production)
+        
+        
+        
         try {
             // Load the JDBC driver
             Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             throw new SQLException("JDBC Driver not found", e);
         }
-        return DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
+
+        return DriverManager.getConnection(DB_URL, DB_USER, dbPassword);
     }
 }
