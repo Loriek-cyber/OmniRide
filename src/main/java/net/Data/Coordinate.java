c629c9adc7f6bae35fdbaa86012057928441e900
Loriek-cyber <arjel.buzi1@gmail.com>
2025-06-25 16:51:16 +0200

    Initial commit: OmniRide project setup

---
+++ src/main/java/net/Data/Coordinate.java
@@ -0,0 +1,27 @@
+package net.Data;
+
+public class Coordinate {
+    private double Latitudine;
+    private double Longitude;
+
+    public Coordinate(double lat, double lon) {
+        this.Latitudine = lat;
+        this.Longitude = lon;
+    }
+
+    public double getLatitudine() {
+        return Latitudine;
+    }
+
+    public void setLatitudine(double latitudine) {
+        Latitudine = latitudine;
+    }
+
+    public double getLongitude() {
+        return Longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        Longitude = longitude;
+    }
+}
